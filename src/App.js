a09a00578b02e625bfc9f8ed286c9fd03aef3bcc
anderson.cvlh <anderson.cvlh@gmail.com>
2018-02-25 11:00:14 -0300

    initial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import 'bootstrap-css-only';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Comments System</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p><button className="btn btn-info" >Test</button></p>
+      </div>
+    );
+  }
+}
+
+export default App;
