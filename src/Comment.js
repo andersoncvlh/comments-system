18d83d8ecd772149714d5fc304b746e130dc7cc8
anderson.cvlh <anderson.cvlh@gmail.com>
2018-03-01 22:19:19 -0300

    Functional Stateless Component

--- src/Comment.js
+++ src/Comment.js
@@ -1,17 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Comment extends Component {
-  render() {
-    return (
-      <div className="card" >
-        <div className="card-body">
-          <h5 className="card-title">Comment title - Ex: Who's?</h5>
-          <p className="card-text">{this.props.comment.comment}</p>
-          <a className="btn btn-primary">Action... like... Reply??</a>
-        </div>
+const Comment = (props) => {
+  return (
+    <div className="card" >
+      <div className="card-body">
+        <h5 className="card-title">Comment title - Ex: Who's?</h5>
+        <p className="card-text">{props.comment.comment}</p>
+        <a className="btn btn-primary">Action... like... Reply??</a>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Comment;
